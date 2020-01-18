4a1e9287b768855be76e1c5c5a8560560f413fae
Landon Lyle <landonlyle@live.ca>
2020-01-17 23:11:26 -0500

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2JanCompleted.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Products()
+        {
+            string[] products = { "Laptop", "Tablet", "Desktop" };
+
+            ViewData["products"] = products;
+
+            return View();
+
+        }
+        public ActionResult Details(string product)
+        {
+            ViewBag.Message = "You have selected " + product;
+            return View();
+
+
+        }
+    }
+
+}
